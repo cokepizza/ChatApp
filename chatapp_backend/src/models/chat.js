d9ef5b69b5042e6d52daaff8e85482d3c5407b02
cokepizza <cokepizza3@gmail.com>
2020-03-17 00:06:35 +0900

    [backend] chat-user manytomany 로 작업 예정

--- chatapp_backend/src/models/chat.js
+++ chatapp_backend/src/models/chat.js
@@ -2,19 +2,19 @@ import { DataTypes } from 'sequelize';
 
 const chat = sequelize => {
     const Chat = sequelize.define('chat', {
-        imagename: {
-            type: DataTypes.STRING(200),
-            allowNull: false,
-            unique: true,   
-        },
-        order: {
-            type : DataTypes.INTEGER.UNSIGNED,
-            allowNull : false,
-        },
-        size: {
-            type : DataTypes.INTEGER.UNSIGNED,
-            allowNull : false,
-        }
+        // imagename: {
+        //     type: DataTypes.STRING(200),
+        //     allowNull: false,
+        //     unique: true,   
+        // },
+        // order: {
+        //     type : DataTypes.INTEGER.UNSIGNED,
+        //     allowNull : false,
+        // },
+        // size: {
+        //     type : DataTypes.INTEGER.UNSIGNED,
+        //     allowNull : false,
+        // }
     }, {
         timestamps: true,
         paranoid: true,
