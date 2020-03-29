534e14c131bffa64357fbfe77aace7c14b680a0b
cokepizza <cokepizza3@gmail.com>
2020-03-30 04:26:16 +0900

    [native] build static 위치 수정

--- chatapp_backend/src/main.js
+++ chatapp_backend/src/main.js
@@ -4,6 +4,8 @@ import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import cors from 'cors';
 import passport from 'passport';
+import path from 'path';
+
 import api from './api';
 import socket from './socket';
 import passportConfig from './passport';
@@ -14,6 +16,8 @@ dotenv.config();
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
+app.use("/tableEditor", express.static(__dirname + '../../../spa/TableEditor/build'));
+app.use("/layoutEditor", express.static(__dirname + '../../../spa/LayoutEditor_Boilerplate/layout_frontend/build'));
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
